3c8b543b257534919b76ede5fec240148423f41d
drewsteels <njugunaak@gmail.com>
2024-09-08 21:26:53 -0400

    made  corrections to djangoproj/urls.py and djangoapp/urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('logout/', TemplateView.as_view(template_name="home.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
